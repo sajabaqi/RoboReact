f2623db5167a6a2f48f98a8ec8ec3f2744fdc46b
saja abubaqi <sajaabubaqi59@gmail.com>
2022-02-16 17:38:13 +0200

    Updates

---
+++ precache-manifest.90e5d439b469a12b22ae85774abd716b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3248896e20e16dfe375e6944ac5592b6",
+    "url": "/RoboReact/index.html"
+  },
+  {
+    "revision": "590b2a58f38c125bde60",
+    "url": "/RoboReact/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "6eb6e80b1d30a6499f44",
+    "url": "/RoboReact/static/css/main.1f323d74.chunk.css"
+  },
+  {
+    "revision": "590b2a58f38c125bde60",
+    "url": "/RoboReact/static/js/2.9858653f.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/RoboReact/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "6eb6e80b1d30a6499f44",
+    "url": "/RoboReact/static/js/main.32007151.chunk.js"
+  },
+  {
+    "revision": "081e14fc104ddf7f0cae",
+    "url": "/RoboReact/static/js/runtime~main.b0320569.js"
+  }
+]);
